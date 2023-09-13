9052b63522249a19b1134df077aa1b25dcb8ede9
Gourav12222 <gourav.zeroit@gmail.com>
2023-09-13 16:10:11 +0530

    Add the pages

---
+++ admin/application/routes/login.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const {
+  registerView,
+  loginView,
+  
+} = require("../controllers/loginController");
+const { dashboardView } = require("../controllers/dashboardController");
+const { protectRoute } = require("../auth/protect");
+
+const router = express.Router();
+
+router.get("/register", registerView);
+router.get("/login", loginView);
+//Dashboard
+router.get("/dashboard", protectRoute, dashboardView);
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+module.exports = router;
