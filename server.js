29c1914cb739808fe1de72d530f4333792960619
mohitdhiman.zeroit <mohitdhiman.zeroit@gmail.com>
2023-09-15 12:37:20 +0530

    Added on view path in server page

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const session = require('express-session');
-
+const path = require('path');
 const passport = require("passport");
 const { loginCheck } = require("./admin/application/auth/passport");
 loginCheck(passport);
 
+
+
 // Mongo DB conncetion
-const database = "mongodb+srv://mohitdhimanzeroit:Mohit@023_@cluster0.0pw1i1q.mongodb.net/"
+const database = "mongodb+srv://mohitdhimanzeroit:Mohit%40023_@cluster0.0pw1i1q.mongodb.net/"
 
 mongoose
   .connect(database, { useUnifiedTopology: true, useNewUrlParser: true })
@@ -17,7 +19,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
-
+app.set('views', path.join(__dirname, 'views'));
 //BodyParsing
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
@@ -32,6 +34,6 @@ app.use(passport.session());
 //Routes
 app.use("/", require("./admin/application/routes/login"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4111;
 
 app.listen(PORT, console.log("Server has started at port " + PORT));
